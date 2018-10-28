791812396c1c9594b86111df791675318fbbd12b
Reb Sen <archivesreb@gmail.com>
2018-10-28 10:53:35 +0100

    ajout d'un h1 ligne 18 dans app branch master

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ class App extends Component {
       <Container className="App">
         <Row>
           <Col sm={{ size: 10, offset: 1 }} md={{ size: 6, offset:3  }} className="shape"><Cardos/></Col>
-          
         </Row>
-      </Container>
+        <h1>APP DE MASTER VOYONS VOIR ...</h1>
+     </Container>
+
+      
     );
   }
 }
