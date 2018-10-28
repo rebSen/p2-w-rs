74e2c70aa666b20f0844302de1c685df766922f4
Reb Sen <archivesreb@gmail.com>
2018-10-28 10:55:30 +0100

    ajout d'un H1 dans le app de la branch search bar test avant merge

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ import Search from './Search'
 import Cardos from './Cardos'
 
 
-
 class App extends Component {
   render() {
     return (
       <div>
-        
-       
      <Container className="">
       <Search/>
       <Row>  
         <Col sm={{ size: 10, offset: 1 }} md={{ size: 8, offset:4 }} className="shape"><Cardos/></Col>
       </Row>
+      <h1>APP DE SEARCHBAR VOYONS VOIR !!! : )</h1>
     </Container>
       
       </div>
